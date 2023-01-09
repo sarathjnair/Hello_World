73cf483b5631867e21cd80de00264964f3501c9a
sarathjnair <sarathjnair99@gmail.com>
2023-01-09 15:02:03 +0530

    modified program for accepting only integer inputs

--- Demo_folder/user_calculator.py
+++ Demo_folder/user_calculator.py
@@ -1,4 +1,6 @@
-a=int(input("Enter number 1 : "))
-b=int(input("Enter number 2 : "))
-
-print(f"The sum of {a} and {b} is {a+b}")
+a=input("Enter number 1 : ")
+b=input("Enter number 2 : ")
+if(type(a).__name__=="int" and type(b).__name__ == "int"):
+    print(f"The sum of {a} and {b} is {int(a)+int(b)}")
+else:
+    print("Entered inputs are not integers")
