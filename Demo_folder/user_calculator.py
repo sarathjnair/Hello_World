4fe06b81efdae1f4bfe918b744112e3d761b504e
sarathjnair <sarathjnair99@gmail.com>
2023-01-09 15:06:42 +0530

    updating the user_calculator.py with bugfix for integer inputs

--- Demo_folder/user_calculator.py
+++ Demo_folder/user_calculator.py
@@ -1,5 +1,7 @@
 a=input("Enter number 1 : ")
 b=input("Enter number 2 : ")
+a=int(a)
+b=int(b)
 if(type(a).__name__=="int" and type(b).__name__ == "int"):
     print(f"The sum of {a} and {b} is {int(a)+int(b)}")
 else:
